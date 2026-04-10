d26675da0a0979e5f0b5307a452b62be2a2282da
spofoh <spofoh@gmail.com>
2026-04-10 11:31:34 +0200

    removed the intergrations

--- src/main/java/net/envexus/svcmute/integrations/MuteChecker.java
+++
@@ -1,8 +0,0 @@
-package net.envexus.svcmute.integrations;
-
-import org.bukkit.entity.Player;
-
-public interface MuteChecker {
-    boolean isPlayerMuted(Player player);
-    long getUnmuteTime(Player player);
-}
